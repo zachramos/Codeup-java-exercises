934ec39980f239ce2d84149c471487dfbeb81d4b
zacharyramos <zachary.ramos9@gmail.com>
2018-02-27 12:03:30 -0600

    learned and used some basic syntax, types and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello");System.out.print(" World!");
+        int myFavoriteNumber = 26;
+            System.out.println("My favorite number is:" + myFavoriteNumber);
+        String myString = "This is a string.";
+            System.out.println(myString);
+        float myNumber = 3.14f;
+            System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        //top^ increments after and bottom increments before
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+    //shorthand examples
+        //addition
+//            int x = 4;
+//            x += 5;
+        //multiply
+//            int x = 3;
+//            int y = 4;
+//            y *= x;
+        //divide and subtract
+//            int x = 10;
+//            int y = 2;
+//            x /= y;
+//            y -= x;
+
     }
 }
